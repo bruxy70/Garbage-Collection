3a199c0bbeeead9a0d6a267497e9d3eb0c79051f
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2020-06-04 08:53:09 +0200

    version

--- custom_components/garbage_collection/const.py
+++ custom_components/garbage_collection/const.py
@@ -8,7 +8,7 @@ from homeassistant.const import CONF_NAME, WEEKDAYS, CONF_ENTITIES
 # Base component constants
 DOMAIN = "garbage_collection"
 CALENDAR_NAME = "Garbage Collection Calendar"
-VERSION = "3.00-beta3"
+VERSION = "3.00-beta4"
 SENSOR_PLATFORM = "sensor"
 CALENDAR_PLATFORM = "calendar"
 ISSUE_URL = "https://github.com/bruxy70/Garbage-Collection/issues"
