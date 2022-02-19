9a0a35733ed00ea09ebc681279eb86ac25c86536
bruxy70 <vasek.chaloupka@hotmail.com>
2022-02-19 10:46:58 +0100

    test all frequencies

--- test/test_frequencies.py
+++ test/test_frequencies.py
@@ -1,9 +1,13 @@
-"""Test calendar for simple integration."""
+"""Test all frequencies (except blank)."""
 from datetime import date
 
 from custom_components.garbage_collection import const
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+ERROR_DAYS = "Next collection should be in {} days, not {}"
+ERROR_STATE = "State should be {}, not {}"
+ERROR_DATE = "Next collection date shoudl be {}, not {}"
 
 
 async def test_weekly(hass: HomeAssistant):
@@ -22,11 +26,11 @@ async def test_weekly(hass: HomeAssistant):
     state = sensor.state
     days = sensor.attributes["days"]
     next_date = sensor.attributes["next_date"]
-    assert state == "2", f"State should be 2, not {state}"
-    assert days == 5, f"Should be in 5 dats, not {days}"
-    assert next_date.date() == date(
-        2020, 4, 6
-    ), f"Next collection should be April 6, 2020, not {next_date}"
+    assert state == "2", ERROR_STATE.format(2, state)
+    assert days == 5, ERROR_DAYS.format(5, days)
+    assert next_date.date() == date(2020, 4, 6), ERROR_DATE.format(
+        "April 6, 2020", next_date
+    )
 
 
 async def test_odd_weeks(hass: HomeAssistant):
@@ -45,9 +49,11 @@ async def test_odd_weeks(hass: HomeAssistant):
     state = sensor.state
     days = sensor.attributes["days"]
     next_date = sensor.attributes["next_date"]
-    assert state == "2", f"State should be 2, not {state}"
-    assert days == 6, f"Should be in 6 days, not {days}"
-    assert next_date.date() == date(2020, 4, 7), f"Should be April 7, not {next_date}"
+    assert state == "2", ERROR_STATE.format(2, state)
+    assert days == 6, ERROR_DAYS.format(5, days)
+    assert next_date.date() == date(2020, 4, 7), ERROR_DATE.format(
+        "April 7, 2020", next_date
+    )
 
 
 async def test_even_weeks(hass: HomeAssistant):
@@ -66,63 +72,67 @@ async def test_even_weeks(hass: HomeAssistant):
     state = sensor.state
     days = sensor.attributes["days"]
     next_date = sensor.attributes["next_date"]
-    assert state == "0", f"State should be 0, not {state}"
-    assert days == 0, f"Days should be 0, not {days}"
-    assert next_date.date() == date(
-        2020, 4, 1
-    ), f"Next date should be April 1, not {next_date}"
+    assert state == "0", ERROR_STATE.format(0, state)
+    assert days == 0, ERROR_DAYS.format(0, days)
+    assert next_date.date() == date(2020, 4, 1), ERROR_DATE.format(
+        "April 1, 2020", next_date
+    )
 
 
-# async def test_every_n_days(hass: HomeAssistant):
-#     """Every n days collection."""
+async def test_every_n_days(hass: HomeAssistant):
+    """Every n days collection."""
 
-#     config_entry: MockConfigEntry = MockConfigEntry(
-#         domain=const.DOMAIN,
-#         data={
-#             "frequency": "every-n-days",
-#             "period": 14,
-#             "first-date": "2020-01-01",
-#         },
-#         title="sensor",
-#         version=4.5,
-#     )
-#     config_entry.add_to_hass(hass)
-#     await hass.config_entries.async_setup(config_entry.entry_id)
-#     await hass.async_block_till_done()
-#     sensor = hass.states.get("sensor.sensor")
-#     state = sensor.state
-#     days = sensor.attributes["days"]
-#     next_date = sensor.attributes["next_date"]
-#     assert state == "2", f"State should be 2, not {state}"
-#     assert days
-#     assert next_date.date()
+    config_entry: MockConfigEntry = MockConfigEntry(
+        domain=const.DOMAIN,
+        data={
+            "frequency": "every-n-days",
+            "period": 14,
+            "first_date": "2020-01-01",
+        },
+        title="sensor",
+        version=4.5,
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    sensor = hass.states.get("sensor.sensor")
+    state = sensor.state
+    days = sensor.attributes["days"]
+    next_date = sensor.attributes["next_date"]
+    assert state == "2", ERROR_STATE.format(2, state)
+    assert days == 7, ERROR_DAYS.format(7, days)
+    assert next_date.date() == date(2020, 4, 8), ERROR_DATE.format(
+        "April 8, 2020", next_date
+    )
 
 
-# async def test_every_n_weeks(hass: HomeAssistant):
-#     """Every n weeks collection."""
+async def test_every_n_weeks(hass: HomeAssistant):
+    """Every n weeks collection."""
 
-#     config_entry: MockConfigEntry = MockConfigEntry(
-#         domain=const.DOMAIN,
-#         data={
-#             "frequency": "every-n-weeks",
-#             "period": 2,
-#             "first-week": 3,
-#             "weekday-order-number": ["3"]
-#             "collection_days": ["wed"],
-#         },
-#         title="sensor",
-#         version=4.5,
-#     )
-#     config_entry.add_to_hass(hass)
-#     await hass.config_entries.async_setup(config_entry.entry_id)
-#     await hass.async_block_till_done()
-#     sensor = hass.states.get("sensor.sensor")
-#     state = sensor.state
-#     days = sensor.attributes["days"]
-#     next_date = sensor.attributes["next_date"]
-#     assert state == "2", f"State should be 2, not {state}"
-#     assert days
-#     assert next_date.date()
+    config_entry: MockConfigEntry = MockConfigEntry(
+        domain=const.DOMAIN,
+        data={
+            "frequency": "every-n-weeks",
+            "period": 2,
+            "first_week": 3,
+            "weekday-order-number": ["3"],
+            "collection_days": ["wed"],
+        },
+        title="sensor",
+        version=4.5,
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    sensor = hass.states.get("sensor.sensor")
+    state = sensor.state
+    days = sensor.attributes["days"]
+    next_date = sensor.attributes["next_date"]
+    assert state == "2", ERROR_STATE.format(2, state)
+    assert days == 7, ERROR_DAYS.format(7, days)
+    assert next_date.date() == date(2020, 4, 8), ERROR_DATE.format(
+        "April 8, 2020", next_date
+    )
 
 
 async def test_monthly(hass: HomeAssistant):
@@ -145,11 +155,11 @@ async def test_monthly(hass: HomeAssistant):
     state = sensor.state
     days = sensor.attributes["days"]
     next_date = sensor.attributes["next_date"]
-    assert state == "2", f"State should be 2, not {state}"
-    assert days == 9, f"Days should be 9, not {days}"
-    assert next_date.date() == date(
-        2020, 4, 10
-    ), f"Next date should be April 10, not {next_date}"
+    assert state == "2", ERROR_STATE.format(2, state)
+    assert days == 9, ERROR_DAYS.format(9, days)
+    assert next_date.date() == date(2020, 4, 10), ERROR_DATE.format(
+        "April 10, 2020", next_date
+    )
 
 
 async def test_monthly2(hass: HomeAssistant):
@@ -173,11 +183,11 @@ async def test_monthly2(hass: HomeAssistant):
     state = sensor.state
     days = sensor.attributes["days"]
     next_date = sensor.attributes["next_date"]
-    assert state == "2", f"State should be 2, not {state}"
-    assert days == 5, f"Days should be 5, not {days}"
-    assert next_date.date() == date(
-        2020, 4, 6
-    ), f"Next date should be April 6, not {next_date}"
+    assert state == "2", ERROR_STATE.format(2, state)
+    assert days == 5, ERROR_DAYS.format(5, days)
+    assert next_date.date() == date(2020, 4, 6), ERROR_DATE.format(
+        "April 6, 2020", next_date
+    )
 
 
 async def test_annual(hass: HomeAssistant):
@@ -199,8 +209,8 @@ async def test_annual(hass: HomeAssistant):
     state = sensor.state
     days = sensor.attributes["days"]
     next_date = sensor.attributes["next_date"]
-    assert state == "2", f"State should be 2, not {state}"
-    assert days == 30, f"Days should be 30, not {days}"
-    assert next_date.date() == date(
-        2020, 5, 1
-    ), f"Next date should be May 1, not {next_date}"
+    assert state == "2", ERROR_STATE.format(2, state)
+    assert days == 30, ERROR_DAYS.format(30, days)
+    assert next_date.date() == date(2020, 5, 1), ERROR_DATE.format(
+        "May 1, 2020", next_date
+    )
