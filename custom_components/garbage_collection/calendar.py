232c4deab74c9888635488e6e2ea383b4e1757e4
bruxy70 <vasek.chaloupka@hotmail.com>
2022-07-13 18:25:08 +0200

    Docstle

--- custom_components/garbage_collection/calendar.py
+++ custom_components/garbage_collection/calendar.py
@@ -18,7 +18,7 @@ async def async_setup_entry(
     _: HomeAssistant, config_entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     # pylint: disable=unused-argument
-    """Add calendar entity to HA"""
+    """Add calendar entity to HA."""
     async_add_entities([GarbageCollectionCalendar()], True)
 
 
